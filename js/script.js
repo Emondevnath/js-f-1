627effef196362106bf79c0688e858f16e4ba960
Emon Nath <138616621+Emondevnath@users.noreply.github.com>
2024-08-02 13:34:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/*example one*/
+
+function showText(){
+   document.getElementById('s_text').innerHTML ="Hello javascript";
+}
+
+
+// example two
+
+function showTextTwo(){
+    document.getElementById('s_text_two').innerHTML ="Hello world";
+}
+
+// example three
+
+function showTextThree(){
+    document.getElementById('s_text_three').innerHTML ="My Name is Emon nath";
+}
+
+// example four
+
+function showTextFour(){
+    document.getElementById('s_text_four').innerHTML ="I Learn Javascript";
+}
+
+// example five
+
+function showTextFive(){
+    document.getElementById('s_text_five').innerHTML ="I learn programming language";
+}
+
+// example six
+
+function showTextSix(){
+    document.getElementById('s_text_six').innerHTML ="I learn programming language";
+}
+
+// example seven
+
+function showTextSeven(){
+    document.getElementById('s_text_seven').innerHTML ="I learn web development";
+}
+
+// example eight
+
+function showTextEight(){
+    document.getElementById('s_text_eight').innerHTML ="I learn wordpress";
+}
+
+// example nine
+
+function showTextNine(){
+    document.getElementById('s_text_nine').innerHTML ="I learn jquery";
+}
+
+// example ten
+
+function showTextTen(){
+    document.getElementById('s_text_ten').innerHTML ="I am future web developer";
+}
